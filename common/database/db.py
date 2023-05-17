81337eb49be8807aaee932ab8ba985c4c827b811
김형준 <khjkhj2804@naver.com>
2023-05-17 10:30:18 +0900

    Update db.py -> sync to async DB

--- common/database/db.py
+++ common/database/db.py
@@ -37,15 +37,18 @@ __CDB_ASYNC_SESSION = async_scoped_session(sessionmaker(__CDB_ENGINE, class_=Asy
 
 
 __redisInfo: RedisConfig = CONFIG.GetConfig(RedisConfig)
-__REDIS = get_redis_connection(
-    host=__redisInfo.host,
-    port=__redisInfo.port,
-    decode_responses=True
-)
+# __REDIS = get_redis_connection(
+#     host=__redisInfo.host,
+#     port=__redisInfo.port,
+#     decode_responses=True
+# )
+__REDIS = Redis(host=__redisInfo.host, port=__redisInfo.port, decode_responses=True)
 
 
 __mdbInfo:MongoDBConfig = CONFIG.GetConfig(MongoDBConfig)
-__MONGO_DB = MongoClient(host=__mdbInfo.host, port=__mdbInfo.port)
+#__MONGO_DB = MongoClient(host=__mdbInfo.host, port=__mdbInfo.port)
+__MONGO_DB = AsyncIOMotorClient(__mdbInfo.host, __mdbInfo.port) #(f"mongodb://{__mdbInfo.host}:{__mdbInfo.port}/")
+
 
 
 async def __InitModel():
@@ -57,9 +60,67 @@ async def __InitModel():
 async def GetCDB() -> AsyncSession:
     async with __CDB_ASYNC_SESSION() as s:
         yield s
+        
+async def CDBExcute(db:AsyncSession, query, errMsg="Contents DB Excute Failed", raiseError=True) -> tuple[ErrorType, list]:
+    try:
+        res = await db.execute(query)
+        if hasattr(res, 'rowcount') == False:
+            return ErrorType.SUCCESS, res.scalars().fetchall()
+    
+        await db.commit()
+    except:
+        await db.rollback()
+        LOG.e(ErrorType.DB_RUN_FAILED.name, errMsg)
+        
+        if raiseError:
+            raise RuntimeError(ErrorType.DB_RUN_FAILED.name, errMsg)
+        else:
+            return ErrorType.DB_RUN_FAILED, []
+    
+    return ErrorType.SUCCESS, []
+
+async def CDBRun(db:AsyncSession, errMsg="Contents DB Run Failed", raiseError=True) -> ErrorType:
+    try:
+        await db.commit()
+    except:
+        await db.rollback()
+        LOG.e(ErrorType.DB_RUN_FAILED.name, errMsg)
+        
+        if raiseError:
+            raise RuntimeError(ErrorType.DB_RUN_FAILED.name, errMsg)
+        else:
+            return ErrorType.DB_RUN_FAILED
+    
+    return ErrorType.SUCCESS
+    
+
+async def CDBJob_Add(db:AsyncSession, query, errMsg="DBJob Add Failed", raiseError=True) -> ErrorType:
+    try:
+        res = await db.execute(query)
+        if hasattr(res, 'rowcount') == False:
+            raise RuntimeError(ErrorType.DB_RUN_FAILED.name, errMsg + " DO NOT USE SELECT QUERY IN DBJOB")
+        
+    except:
+        await db.rollback()
+        LOG.e(ErrorType.DB_RUN_FAILED.name, errMsg)
+        
+        if raiseError:
+            raise RuntimeError(ErrorType.DB_RUN_FAILED.name, errMsg)
+        else:
+            return ErrorType.DB_RUN_FAILED
+    
+    return ErrorType.SUCCESS
 
 async def GetMDB():
     return __MONGO_DB[__mdbInfo.name]
 
-async def GetRedis() -> Redis:
-    return __REDIS
+async def GetMDB_Session():
+    return await __MONGO_DB.start_session()
+    
+async def GetRedis() -> Pipeline:
+    
+    async with __REDIS.pipeline(transaction=False) as pipe:
+        yield pipe
+    
+    #return __REDIS
+
